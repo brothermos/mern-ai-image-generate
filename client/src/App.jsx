cb88662ae59c5b8547684ba0ccd15713b338bf3a
maaammos <moss10612@gmail.com>
2023-01-25 16:41:04 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+import { Header } from "./components";
+import { Home, CreatePost } from "./pages";
+
+const App = () => (
+    <BrowserRouter>
+        <Header/>
+        <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/create-post" element={<CreatePost />} />
+            </Routes>
+        </main>
+    </BrowserRouter>
+);
+
+export default App;
